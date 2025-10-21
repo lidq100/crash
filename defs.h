d1a9cb87a73f8dfeb68933256d21f59d1c83a000
dongqing.li <dongqing.li@amlogic.com>
2025-10-21 09:59:52 +0800

    Merge all master updates since a312f58feb0 into amlogic
    src branch: amlogic
    dst branch: crash master
    Date: 2025-10-07
    Version: V9.0.1
    GDB ver: gdb-16.2
    Commit ID: c62b31700e700251f
    Commit Info: Fix "mount" MNT_CURSOR entries...
    
    How to sync updates from the master branch next time:
    git pull
    git checkout -b temp_merge abc1234
    git merge --squash origin/master
    git commit -m "Merge all master updates since abc1234"
    git checkout amlogic
    git merge temp_merge
    git branch -D temp_merge

--- defs.h
+++ defs.h
@@ -1083,7 +1083,7 @@ struct machdep_table {
         void (*get_irq_affinity)(int);
         void (*show_interrupts)(int, ulong *);
 	int (*is_page_ptr)(ulong, physaddr_t *);
-	int (*get_current_task_reg)(int, const char *, int, void *);
+	int (*get_current_task_reg)(int, const char *, int, void *, int);
 	int (*is_cpu_prstatus_valid)(int cpu);
 };
 
@@ -2273,6 +2273,11 @@ struct offset_table {                    /* stash of commonly-used offsets */
 	long task_struct_thread_context_x28;
 	long neigh_table_hash_heads;
 	long neighbour_hash;
+	long request_queue_tag_set;
+	long blk_mq_tag_set_flags;
+	long blk_mq_tag_set_shared_tags;
+	long vfsmount_mnt_flags;
+	long proc_mounts_cursor;
 };
 
 struct size_table {         /* stash of commonly-used sizes */
@@ -2450,6 +2455,7 @@ struct size_table {         /* stash of commonly-used sizes */
 	long fred_frame;
 	long vmap_node;
 	long cpumask_t;
+	long task_struct_exit_state;
 };
 
 struct array_table {
@@ -5510,6 +5516,7 @@ void cmd_s390dbf(void);
 #endif
 void cmd_map(void);          /* kvmdump.c */
 void cmd_ipcs(void);         /* ipcs.c */
+void cmd_rustfilt(void);     /* symbols.c */
 
 /*
  *  main.c
@@ -6117,6 +6124,7 @@ extern char *help_wr[];
 extern char *help_s390dbf[];
 #endif
 extern char *help_map[];
+extern char *help_rustfilt[];
 
 /*
  *  task.c
@@ -8329,5 +8337,6 @@ enum ppc64_regnum {
 
 /* crash_target.c */
 extern int gdb_change_thread_context (void);
+extern int gdb_add_substack (int);
 
 #endif /* !GDB_COMMON */
--- symbols.c
+++ symbols.c
@@ -22,6 +22,7 @@
 #include "config.h"
 #endif
 #include "bfd.h"
+#include "demangle.h"
 
 static void store_symbols(bfd *, int, void *, long, unsigned int);
 static void store_sysmap_symbols(void);
@@ -3280,6 +3281,46 @@ load_module_filter(char *s, int type)
 
 #define AVERAGE_SYMBOL_SIZE (16)
 
+static size_t rust_demangle_symbol(const char *symbol, char *out, size_t out_size)
+{
+	int i;
+	size_t loc = 0;
+	size_t len = strlen(symbol);
+	char *buf = NULL;
+	/*
+	 * Rust symbols always start with _R (v0) or _ZN (legacy)
+	 */
+	const char *mangled_rust[] = {
+		"_R",
+		"_ZN",
+		NULL
+	};
+
+	if (!out || out_size < len)
+		return 0;
+
+	for (i = 0; mangled_rust[i]; i++) {
+		size_t sz = strlen(mangled_rust[i]);
+		char *p = memmem(symbol, len, mangled_rust[i], sz);
+		if (p) {
+			loc = p - symbol;
+			if (loc)
+				memcpy(out, symbol, loc);
+			break;
+		}
+	}
+
+	buf = rust_demangle(symbol + loc, DMGL_RUST);
+	if (buf) {
+		memcpy(out + loc, buf, strlen(buf));
+		free(buf);
+		return 1;
+	} else if (loc != 0)
+		memset(out, 0, loc);
+
+	return 0;
+}
+
 static int
 namespace_ctl(int cmd, struct symbol_namespace *ns, void *nsarg1, void *nsarg2)
 {
@@ -3288,6 +3329,7 @@ namespace_ctl(int cmd, struct symbol_namespace *ns, void *nsarg1, void *nsarg2)
 	char *name;
 	long cnt;
 	int len;
+	char demangled[BUFSIZE] = {0};
 
 	switch (cmd)
 	{
@@ -3321,6 +3363,10 @@ namespace_ctl(int cmd, struct symbol_namespace *ns, void *nsarg1, void *nsarg2)
 		sp = (struct syment *)nsarg1;
 		name = (char *)nsarg2;
 		len = strlen(name)+1;
+		if (rust_demangle_symbol(name, demangled, sizeof(demangled))) {
+			len = strlen(demangled) + 1;
+			name = demangled;
+		}
 		if ((ns->index + len) >= ns->size) { 
                         if (!(addr = realloc(ns->address, ns->size*2))) 
 				error(FATAL, "symbol name space malloc: %s\n",
@@ -4543,7 +4589,7 @@ is_shared_object(char *file)
 
 		case EM_X86_64:
 			if (machine_type("X86_64") || machine_type("ARM64") ||
-			    machine_type("PPC64"))
+			    machine_type("PPC64") || machine_type("RISCV64"))
 				return TRUE;
 			break;
 
@@ -4888,6 +4934,38 @@ do_multiples:
                 } while(args[optind]);
         }
         else if (!others) 
+		cmd_usage(pc->curcmd, SYNOPSIS);
+}
+
+/*
+ * Demangle a mangled Rust symbol to human readable symbol
+ */
+void cmd_rustfilt(void)
+{
+	int c;
+
+	while ((c = getopt(argcnt, args, "")) != EOF) {
+		switch(c)
+		{
+		default:
+			argerrs++;
+			break;
+		}
+	}
+
+	if (argerrs)
+		cmd_usage(pc->curcmd, SYNOPSIS);
+
+	if (args[optind]) {
+		char *buf;
+
+		buf = rust_demangle(args[optind], DMGL_RUST);
+		if (buf) {
+			fprintf(fp, "%s", buf);
+			free(buf);
+		} else
+			fprintf(fp, "Not a rust symbol: \n%s", args[optind]);
+	} else
 		cmd_usage(pc->curcmd, SYNOPSIS);
 }
 
@@ -10617,6 +10695,10 @@ dump_offset_table(char *spec, ulong makestruct)
 		OFFSET(vfsmount_mnt_mountpoint));
         fprintf(fp, "           vfsmount_mnt_parent: %ld\n", 
 		OFFSET(vfsmount_mnt_parent));
+	fprintf(fp, "            vfsmount_mnt_flags: %ld\n",
+		OFFSET(vfsmount_mnt_flags));
+	fprintf(fp, "            proc_mounts_cursor: %ld\n",
+		OFFSET(proc_mounts_cursor));
 	fprintf(fp, "              mount_mnt_parent: %ld\n",
 		OFFSET(mount_mnt_parent));
 	fprintf(fp, "          mount_mnt_mountpoint: %ld\n",
@@ -11491,6 +11573,12 @@ dump_offset_table(char *spec, ulong makestruct)
 		OFFSET(blk_mq_tags_nr_reserved_tags));
 	fprintf(fp, "               blk_mq_tags_rqs: %ld\n",
 		OFFSET(blk_mq_tags_rqs));
+	fprintf(fp, "         request_queue_tag_set: %ld\n",
+		OFFSET(request_queue_tag_set));
+	fprintf(fp, "          blk_mq_tag_set_flags: %ld\n",
+		OFFSET(blk_mq_tag_set_flags));
+	fprintf(fp, "    blk_mq_tag_set_shared_tags: %ld\n",
+		OFFSET(blk_mq_tag_set_shared_tags));
 
 	fprintf(fp, "         subsys_private_subsys: %ld\n", OFFSET(subsys_private_subsys));
 	fprintf(fp, "  subsys_private_klist_devices: %ld\n",
@@ -12812,6 +12900,7 @@ calculate_load_order_6_4(struct load_module *lm, bfd *bfd, int dynamic,
 	asymbol *store;
 	asymbol *sym;
 	symbol_info syminfo;
+	bfd_vma secaddr;
 	char *secname;
 	int i, t;
 
@@ -12864,6 +12953,7 @@ calculate_load_order_6_4(struct load_module *lm, bfd *bfd, int dynamic,
 				}
 				if (strcmp(syminfo.name, s1->name) == 0) {
 					secname = (char *)bfd_section_name(sym->section);
+					secaddr = bfd_section_vma(sym->section);
 					break;
 				}
 
@@ -12894,14 +12984,14 @@ calculate_load_order_6_4(struct load_module *lm, bfd *bfd, int dynamic,
 			}
 
 			/* Update the offset information for the section */
-			sec_start = s1->value - syminfo.value;
+			sec_start = s1->value - syminfo.value + secaddr;
 			/* keep the address instead of offset */
 			lm->mod_section_data[i].addr = sec_start;
 			lm->mod_section_data[i].flags |= SEC_FOUND;
 
 			if (CRASHDEBUG(2))
-				fprintf(fp, "update sec offset sym %s @ %lx  val %lx  section %s\n",
-					s1->name, s1->value, (ulong)syminfo.value, secname);
+				fprintf(fp, "update sec offset sym %s @ %lx  val %lx  section %s @ %lx\n",
+					s1->name, s1->value, (ulong)syminfo.value, secname, secaddr);
 
 			if (strcmp(secname, ".text") == 0)
 				lm->mod_text_start = sec_start;
